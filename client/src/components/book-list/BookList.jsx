1392852cc3356c4a2f04798ebba1762ea0843623
valeYordanov <valio052@gmail.com>
2024-08-04 11:05:31 +0300

    added search functionality in the book list component

--- client/src/components/book-list/BookList.jsx
+++ client/src/components/book-list/BookList.jsx
@@ -6,11 +6,14 @@ import BookListItem from "./book-list-item/BookListItem";
 import BookService from "../../services/bookService";
 
 import { useEffect } from "react";
+
 import Spinner from "../reusables/spinner/Spinner";
 
 export default function BookList() {
   const [books, setBook] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredBooks, setFilteredBooks] = useState([]);
 
   const collectionName = "books";
 
@@ -26,16 +29,30 @@ export default function BookList() {
         );
       } catch (error) {
         console.error(error);
-      } finally {
-        setIsLoading(true);
       }
     };
-
     const minDuration = 1000;
     setTimeout(() => setIsLoading(false), minDuration);
 
     fetchData();
-  }, []);
+  }, [collectionName]);
+
+  useEffect(() => {
+    if (searchQuery === "") {
+      setFilteredBooks(books);
+    } else {
+      const queryLowercased = searchQuery.toLowerCase();
+      const filtered = books.filter((book) =>
+        book.title.toLowerCase().includes(queryLowercased)
+      );
+      setFilteredBooks(filtered);
+    }
+  }, [searchQuery, books]);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <section id="book-list">
       {isLoading ? (
@@ -43,12 +60,26 @@ export default function BookList() {
       ) : (
         <>
           <h1>Browse Books</h1>
+          <div className="search-container">
+            <input
+              type="text"
+              placeholder="Search for a book"
+              value={searchQuery}
+              onChange={handleChange}
+            />
+          </div>
           <div id="card-area">
             <div className="wrapper">
               <div className="box-area">
-                {books.map((book) => (
-                  <BookListItem key={book.id} {...book} />
-                ))}
+                {filteredBooks.length === 0 && searchQuery ? (
+                  <p className="no-book">
+                    Sorry,there is no book matching that name!
+                  </p>
+                ) : (
+                  filteredBooks.map((book) => (
+                    <BookListItem key={book.id} {...book} />
+                  ))
+                )}
               </div>
             </div>
           </div>
