76b5f8460bb4d52a89904a6001b60be336218e75
valeYordanov <162035732+valeYordanov@users.noreply.github.com>
2024-11-09 17:18:06 +0200

    added react icons

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // You can specify custom build options here if needed
+    outDir: 'dist', // Default build output directory
+    sourcemap: true, // Generate source maps for easier debugging
+    rollupOptions: {
+      // Add any rollup-specific options here
+    }
+  },
+});
--- server/config/configDatabase.js
+++ server/config/configDatabase.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function configDatabase() {
-  const connectionString = "mongodb://0.0.0.0:27017/Booklibry";
+  const connectionString = "mongodb+srv://valio052:valio9409081088@cluster0.ht6n65a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
   await mongoose.connect(connectionString);
 
