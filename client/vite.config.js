b2de41e7af97eba7927a3d89bb52538d407eee9f
valeYordanov <162035732+valeYordanov@users.noreply.github.com>
2024-11-16 16:22:27 +0200

    added margin

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // You can specify custom build options here if needed
-    outDir: 'src', // Default build output directory
-    sourcemap: true, // Generate source maps for easier debugging
-    rollupOptions: {
-      // Add any rollup-specific options here
-    },
-    chunkSizeWarningLimit: 1000,
-  },
+  
 });
